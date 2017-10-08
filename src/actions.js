aa608709b5c566902cd0f655cba2bc0058f4bef5
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-10-08 17:05:10 -0300

    Cambios necesarios para deploy

--- src/actions.js
+++ src/actions.js
@@ -2,9 +2,11 @@ import { post } from './httpMethods'
 // action types
 export const SUBMIT_SURVEY = 'SUBMIT_SURVEY'
 
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3000'
+
 // action creators
 export const submitSurvey = (survey) => {
   return dispatch => {
-    post('http://localhost:3000/surveys', survey).then((data) => { console.log(data) })
+    post(`${BACKEND_URL}/surveys`, survey).then((data) => { console.log(data) })
   }
 }
