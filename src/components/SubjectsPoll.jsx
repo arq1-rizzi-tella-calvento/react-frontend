2e34367ea040990581cea597c6520ee282f5fd01
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-12-10 18:58:16 -0300

    Usar opcion seleccionada por defecto en el componente de la encuesta

--- src/components/SubjectsPoll.jsx
+++ src/components/SubjectsPoll.jsx
@@ -6,81 +6,71 @@ import { submitSurvey } from '../actions/subjects';
 import SurveyConfirmation from '../containers/SurveySummary.jsx';
 
 export default class  SubjectsPoll extends Component {
-    state = { subjects: [] }
-    componentDidMount() {
-        surveyOptions(
-            this.props.match.params.token,
-            data => this.setState({ userId: this.props.match.params.token, subjects: data }))
-    }
+  state = { subjects: [] }
+  componentDidMount() {
+    surveyOptions(
+      this.props.match.params.token,
+      data => this.setState({ userId: this.props.match.params.token, subjects: data }))
+  }
 
-    setSelected = (subject) => {
-        return (selected) => {
-            const selectDropdown = selected.target;
-            this.state.subjects.map(subject => {
-                if(subject.name === selectDropdown.name){
-                    subject.selectedChair = selectDropdown.value
-                }
-            });
-        }
-    };
-
-    handleSubmit = (event) => {
-        submitSurvey(
-            this.state,
-            data => render (<SurveyConfirmation msg={data} /> , document.getElementById('root')),
-            data => render(<Alert msg={data.message}/>, document.getElementById('alert'))
-        )
-        event.preventDefault();
+  setSelected = (subject) => {
+    return (selected) => {
+      const selectDropdown = selected.target;
+      let subjectsDup = this.state.subjects;
+      const subjects = subjectsDup.find(subject => subject.name === selectDropdown.name)
+      subject.selectedChair = selectDropdown.value;
+      subject.selected = subject.selectedChair;
+      
+      this.setState({ subjects: subjectsDup });
     }
+  };
 
-    _renderSubject(subject) {
-        return (
-            <div key={subject.id} className='col-md-12 form-group'>
-                <div className='col-md-4'>{subject.name} </div>
-                <div className='col-md-6'>
-                    <select className='form-control' value={this.state.value} onChange={this.setSelected(subject)} name={subject.name} id={subject.id}>
-                        <option value="dont">Todavía no voy a cursar</option>
-                        <option value="cant">No puedo por los horarios</option>
-                        { this._generateChairOptions(subject.chairs)}
-                        <option value="approved">Ya la cursé</option>
-                    </select>
-                </div>
-            </div>
-        )
-    }
+  _renderSubject(subject) {
+    return (
+      <div key={subject.name} className='col-md-6 form-group'>
+        <div className='col-md-12'>{subject.name}</div>
+        <div className='col-md-10'>
+          <select className='form-control' value={subject.selected} onChange={this.setSelected(subject)} name={subject.name} id={subject.name}>
+            <option value="dont">Todavía no voy a cursar</option>
+            <option value="cant">No puedo por los horarios</option>
+            { this._generateChairOptions(subject.chairs)}
+            <option value="approve">Ya la cursé</option>
+          </select>
+        </div>
+      </div>
+    )
+  }
 
-    _generateChairOptions(chairs){
-        return (
-            chairs.map((chair) => {
-                return(
-                    <option value={chair.id}>{chair.time}</option>
-                )
-            })
-        )
-    }
+  _generateChairOptions(chairs) {
+    return chairs.map(chair => <option value={chair.id}>{chair.time}</option>);
+  }
 
-    render() {
-        return(
-            <form onSubmit={ this.handleSubmit }>
-                <div id="alert">
-                </div>
-                <div>
-                    <div className="alert alert-dark">
-                        Ahora decinos que materias tenes pensado cursar!
-                    </div>
-                    <div className="row">
-                        { this.state.subjects.map(subject => {
-                            return(
-                                this._renderSubject(subject)
-                            )
-                        })
-                        }
-                    </div>
-                    <button className="btn btn-sm btn-primary btn-block" type="submit">
-                        Enviar
-                    </button>
-                </div>
-            </form>
-        )
-    }
+  handleSubmit = (event) => {
+    submitSurvey(
+      this.state,
+      data => render (<SurveyConfirmation msg={data} /> , document.getElementById('root')),
+      data => render(<Alert msg={data.message}/>, document.getElementById('alert'))
+    )
+    event.preventDefault();
+  }
+  
+  render() {
+    return(
+      <form onSubmit={ this.handleSubmit }>
+        <div id="alert">
+        </div>
+        <div>
+          <div className="alert alert-dark">
+            Ahora decinos que materias tenes pensado cursar!
+          </div>
+          <div className="row">
+            { this.state.subjects.map(subject => this._renderSubject(subject)) }
+          </div>
+          <button className="btn btn-sm btn-primary btn-block" type="submit">
+            Enviar
+          </button>
+        </div>
+      </form>
+    )
+  }
 }
